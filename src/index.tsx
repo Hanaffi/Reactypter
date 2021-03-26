61fad9c35c99b0dcd6be287e764a09bd0caeb935
Ahmed <ahmedellord56@gmail.com>
2021-03-26 19:02:31 +0200

    restructure

--- src/index.tsx
+++ src/index.tsx
@@ -1,93 +1,82 @@
-import React, { useEffect, useState, useRef } from 'react';
-import ReactDOM from 'react-dom';
 import * as esbuild from 'esbuild-wasm';
-import './index.css';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
 const App = () => {
-    const ref = useRef<any>();
-    const iframe = useRef<any>();
-    const [input, setInput] = useState('');
+  const ref = useRef<any>();
+  const iframe = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-        });
-    };
-    useEffect(() => {
-        startService();
-    }, []);
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+    });
+  };
+  useEffect(() => {
+    startService();
+  }, []);
 
-    const handleSubmit = async () => {
-        if (!ref.current) {
-            return;
-        }
-        iframe.current.srcdoc = html;
-        const result = await ref.current.build({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-            define: {
-                'process.env.NODE_ENV': '"production"',
-                global: 'window'
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    iframe.current.srcdoc = html;
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
+
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+  };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
             }
-        });
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
-        // setCode(result.outputFiles[0].text);
-        iframe.current.contentWindow.postMessage(
-            result.outputFiles[0].text,
-            '*'
-        );
-    };
-    const html = `
-        <html>
-        <head></head>
-        <body>
-            <div id="root"></div>
-            <script>
-                window.addEventListener('message' , (e)=>{
-                    try{
-                        eval(e.data)
-                    }
-                    catch(err){
-                        document.getElementById('root').innerHTML='<div style="color:red;">'+err+'</div>  ' ; 
-                        console.error(err);
-                    }
-                } , false)
-            </script>
-        </body>
-        </html>
-    `;
-
-    return (
-        <div>
-            <textarea
-                onChange={(e) => {
-                    setInput(e.target.value);
-                }}
-                value={input}
-            ></textarea>
-            <div>
-                <button onClick={handleSubmit}>Submit</button>
-            </div>
-            {/* Output */}
-
-            <iframe
-                title="preview"
-                ref={iframe}
-                sandbox="allow-scripts"
-                srcDoc={html}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <CodeEditor />
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+    </div>
+  );
 };
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,32 +1,30 @@
 import * as esbuild from 'esbuild-wasm';
 
 export const unpkgPathPlugin = () => {
-    return {
-        name: 'unpkg-path-plugin',
-        setup(build: esbuild.PluginBuild) {
-            // Entry
-            build.onResolve({ filter: /(^index\.js$)/ }, (args: any) => {
-                return { path: args.path, namespace: 'a' };
-            });
+  return {
+    name: 'unpkg-path-plugin',
+    setup(build: esbuild.PluginBuild) {
+      // Handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: 'index.js', namespace: 'a' };
+      });
 
-            // Relative module
-            build.onResolve({ filter: /^\.+\// }, (args: any) => {
-                return {
-                    namespace: 'a',
-                    path: new URL(
-                        args.path,
-                        'https://unpkg.com' + args.resolveDir + '/'
-                    ).href
-                };
-            });
+      // Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/')
+            .href,
+        };
+      });
 
-            // Main file of the module
-            build.onResolve({ filter: /.*/ }, async (args: any) => {
-                return {
-                    namespace: 'a',
-                    path: `https://unpkg.com/${args.path}`
-                };
-            });
-        }
-    };
+      // Handle main file of a module
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        };
+      });
+    },
+  };
 };
