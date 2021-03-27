0aff2e8014ef7fec135ac8f1b97c7b19c639dec3
Ahmed <ahmedellord56@gmail.com>
2021-03-27 21:09:22 +0200

    Fixing laggy resizing in x-axis

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,14 +1,37 @@
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
+import { useEffect, useState } from 'react';
+
 import './resizable.css';
 interface ResizableProps {
     direction: 'horizontal' | 'vertical';
 }
 const Resizable: React.FC<ResizableProps> = (props) => {
     let resizableProps: ResizableBoxProps;
+    const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+    const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        let timer: any;
+        const listener = () => {
+            if (timer) {
+                clearTimeout(timer);
+            }
+
+            timer = setTimeout(() => {
+                setInnerWidth(window.innerWidth);
+                setInnerHeight(window.innerHeight);
+            }, 400);
+        };
+        window.addEventListener('resize', listener);
+
+        return () => {
+            window.removeEventListener('resize', listener);
+        };
+    }, []);
     if (props.direction === 'horizontal') {
         resizableProps = {
-            maxConstraints: [0.8 * window.innerWidth, Infinity],
-            minConstraints: [0.2 * window.innerWidth, Infinity],
+            maxConstraints: [0.8 * innerWidth, Infinity],
+            minConstraints: [0.2 * innerWidth, Infinity],
             resizeHandles: ['e'],
             height: Infinity,
             width: window.innerWidth * 0.75,
@@ -16,8 +39,8 @@ const Resizable: React.FC<ResizableProps> = (props) => {
         };
     } else {
         resizableProps = {
-            maxConstraints: [Infinity, window.innerHeight * 0.8],
-            minConstraints: [Infinity, window.innerHeight * 0.1],
+            maxConstraints: [Infinity, innerHeight * 0.8],
+            minConstraints: [Infinity, innerHeight * 0.1],
             resizeHandles: ['s'],
             height: 500,
             width: Infinity
