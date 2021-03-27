93760c7ad306013bea2ce1e5cd547243faaddb63
Ahmed <ahmedellord56@gmail.com>
2021-03-27 12:49:17 +0200

    Refactoring 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,19 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
-import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
-
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
 
 import CodeEditor from './components/code-editor';
 import Preview from './components/preview';
+import bundle from './bundler';
 
 const App = () => {
-    const ref = useRef<any>();
     const [input, setInput] = useState('console.log("Hello World");');
     const [code, setCode] = useState('');
 
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-        });
-    };
-    useEffect(() => {
-        startService();
-    }, []);
-
     const onClick = async () => {
-        if (!ref.current) {
-            return;
-        }
-
-        const result = await ref.current.build({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-            define: {
-                'process.env.NODE_ENV': '"production"',
-                global: 'window'
-            }
-        });
-
-        setCode(result.outputFiles[0].text);
+        const output = await bundle(input);
+        setCode(output);
     };
 
     return (
@@ -52,14 +24,10 @@ const App = () => {
                 }}
                 initialValue='console.log("Hello World");'
             />
-            <textarea
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-            ></textarea>
+
             <div>
                 <button onClick={onClick}>Submit</button>
             </div>
-            {/* <pre>{code}</pre> */}
             <Preview code={code} />
         </div>
     );
