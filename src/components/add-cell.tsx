30c2042a5e583215c90d3a9245e290e9181f371d
Ahmed <ahmedellord56@gmail.com>
2021-03-29 01:13:22 +0200

    Styling AddCell

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -7,21 +7,32 @@ interface AddCellProps {
 const AddCell: React.FC<AddCellProps> = (props) => {
     const { insertCellBefore } = useActions();
     return (
-        <div>
-            <button
-                onClick={() => {
-                    insertCellBefore(props.nextCellId, 'code');
-                }}
-            >
-                Code
-            </button>
-            <button
-                onClick={() => {
-                    insertCellBefore(props.nextCellId, 'text');
-                }}
-            >
-                Text
-            </button>
+        <div className="add-cell">
+            <div className="add-buttons">
+                <button
+                    className="button is-rounded is-primary is-small"
+                    onClick={() => {
+                        insertCellBefore(props.nextCellId, 'code');
+                    }}
+                >
+                    <span className="icon is-small">
+                        <i className="fas fa-plus"></i>
+                    </span>
+                    <span>Code</span>
+                </button>
+                <button
+                    className="button is-rounded is-secondary is-small"
+                    onClick={() => {
+                        insertCellBefore(props.nextCellId, 'text');
+                    }}
+                >
+                    <span className="icon is-small">
+                        <i className="fas fa-plus"></i>
+                    </span>
+                    <span>Text</span>
+                </button>
+            </div>
+            <div className="divider"></div>
         </div>
     );
 };
