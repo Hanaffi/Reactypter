06452f31c3453063523bb5286d917f4d388cbbc9
Ahmed <ahmedellord56@gmail.com>
2021-03-28 22:18:23 +0200

    ordered list of cells

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,5 +1,13 @@
+import { useTypedSelector } from '../hooks/use-typed-selector';
+
 const CellList: React.FC = () => {
-    return <div>Cell List</div>;
+    const res = useTypedSelector((state) => {
+        return state.cells?.order.map((id) => {
+            return state.cells?.data[id];
+        });
+    });
+    console.log(res);
+    return <div>xxx</div>;
 };
 
 export default CellList;
