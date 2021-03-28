3c60bcdfb5e54fb05e4fcf4630456b1986a78bd8
Ahmed <ahmedellord56@gmail.com>
2021-03-28 22:41:57 +0200

    useAction

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,9 +1,17 @@
 import { Cell } from '../state';
+import CodeCell from './code-cell';
+import TextEditor from './text-editor';
+
 interface CellListItemProps {
     cell: Cell | undefined;
 }
 const CellListItem: React.FC<CellListItemProps> = (props) => {
-    return <div>{props.cell!.id}</div>;
+    let child: JSX.Element;
+    if (props.cell!.type === 'code') child = <CodeCell />;
+    else {
+        child = <TextEditor />;
+    }
+    return <div>{child}</div>;
 };
 
 export default CellListItem;
--- src/state/index.ts
+++ src/state/index.ts
@@ -3,3 +3,5 @@ export * from './store';
 export * from './reducers';
 
 export * from './cell';
+
+export * as actionCreators from './action-creators';
