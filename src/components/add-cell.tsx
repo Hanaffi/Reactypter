db0b13ba7a2ee80d8155aa89c0fefef252ded0a7
Ahmed <ahmedellord56@gmail.com>
2021-03-29 00:40:20 +0200

    AddCell divider

---
+++ src/components/add-cell.tsx
@@ -0,0 +1,29 @@
+import { useActions } from '../hooks/use-actions';
+import './add-cell.css';
+
+interface AddCellProps {
+    nextCellId: string;
+}
+const AddCell: React.FC<AddCellProps> = (props) => {
+    const { insertCellBefore } = useActions();
+    return (
+        <div>
+            <button
+                onClick={() => {
+                    insertCellBefore(props.nextCellId, 'code');
+                }}
+            >
+                Code
+            </button>
+            <button
+                onClick={() => {
+                    insertCellBefore(props.nextCellId, 'text');
+                }}
+            >
+                Text
+            </button>
+        </div>
+    );
+};
+
+export default AddCell;
