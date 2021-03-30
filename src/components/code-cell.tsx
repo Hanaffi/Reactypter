aab65d9fcf6bedfc93373764aea95195b444de05
Ahmed <ahmedellord56@gmail.com>
2021-03-30 15:03:06 +0200

    Change style of progressBar and fix background bug

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -52,18 +52,20 @@ const CodeCell: React.FC<CodeCellProps> = (props) => {
                         initialValue={props.cell!.content}
                     />
                 </Resizable>
-                {!bundle || bundle.loading ? (
-                    <div className="progress-cover">
-                        <progress
-                            className="progress is-small is-primary"
-                            max="100"
-                        >
-                            Loading
-                        </progress>
-                    </div>
-                ) : (
-                    <Preview code={bundle.code} err={bundle.err} />
-                )}
+                <div className="progress-wrapper">
+                    {!bundle || bundle.loading ? (
+                        <div className="progress-cover">
+                            <progress
+                                className="progress is-small is-primary"
+                                max="100"
+                            >
+                                Loading
+                            </progress>
+                        </div>
+                    ) : (
+                        <Preview code={bundle.code} err={bundle.err} />
+                    )}
+                </div>
             </div>
         </Resizable>
     );
