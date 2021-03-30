1a2f0b53b0eac3d27b30a84acba0567624e24b5e
Ahmed <ahmedellord56@gmail.com>
2021-03-30 12:57:50 +0200

    bundleReducer

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -34,8 +34,26 @@ export interface UpdateCellAction {
     };
 }
 
+export interface BundleStartAction {
+    type: ActionType.BUNDLE_START;
+    payload: {
+        cellId: string;
+    };
+}
+export interface BundleCompleteAction {
+    type: ActionType.BUNDLE_COMPLETE;
+    payload: {
+        cellId: string;
+        bundle: {
+            code: string;
+            err: string;
+        };
+    };
+}
 export type Action =
     | MoveCellAction
     | UpdateCellAction
     | InsertCellAfterAction
-    | DeleteCellAction;
+    | DeleteCellAction
+    | BundleStartAction
+    | BundleCompleteAction;
