ac27306443df2fd2dbef34fe7a12e6b76f2259cf
Ahmed <ahmedellord56@gmail.com>
2021-03-31 20:06:59 +0200

    Error handling in local-api

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -6,7 +6,20 @@ export const serveCommand = new Command()
     .command('serve [filename] ')
     .description('Open a file for editing')
     .option('-p, --port <number>', 'port to run server on', '4005')
-    .action((filename = 'notebook.js', options: { port: string }) => {
-        const dir = path.join(process.cwd(), path.dirname(filename));
-        serve(+options.port, path.basename(filename), dir);
+    .action(async (filename = 'notebook.js', options: { port: string }) => {
+        try {
+            const dir = path.join(process.cwd(), path.dirname(filename));
+            await serve(+options.port, path.basename(filename), dir);
+            console.log(`
+                Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file
+            
+            `);
+        } catch (err) {
+            if (err.code === 'EADDRINUSE') {
+                console.error('Port is in use, try another one please!');
+            } else {
+                console.log('Here is the problem', err.message);
+            }
+            process.exit(1);
+        }
     });
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,7 @@
+import express from 'express';
 export const serve = (port: number, filename: string, dir: string) => {
-    console.log(port, filename, dir);
+    const app = express();
+    return new Promise<void>((resolve, reject) => {
+        app.listen(port, resolve).on('error', reject);
+    });
 };
